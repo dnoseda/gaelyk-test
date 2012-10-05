5c0c4a1bcfc8fa59af34195b09a2e57dbf48aa5d
dnoseda <damian.noseda@mercadolibre.com>
2012-10-05 20:17:25 -0300

    add scripts for build/install the project

---
+++ build_scripts/install.sh
@@ -0,0 +1,5 @@
+mvn archetype:generate -DarchetypeGroupId=org.codeconsole \
+-DarchetypeArtifactId=gaelyk-archetype -DarchetypeVersion=0.7.0 \
+-DartifactId=gaelykapp -DgroupId=com.appspot.gaelyk \
+-DgaeApplicationName=mygaelykapp \
+-DarchetypeRepository=http://maven-gaelyk.googlecode.com/svn/repository/
